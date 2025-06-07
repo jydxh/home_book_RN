408b91b6b2b6ad778fd3500e9ceb025c51163df8
jydxh <wzxxds@gmail.com>
2025-06-07 16:56:34 -0400

    feat: finish the update user info function, validation and toast notifications

--- api/userApi.ts
+++ api/userApi.ts
@@ -1,7 +1,7 @@
 import { backendProxyUrl } from "@/constants/baseUrls";
 import { Profile } from "@/constants/types";
 import { useAuth } from "@clerk/clerk-expo";
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const useFetchUserProfile = () => {
 	const { getToken } = useAuth();
@@ -19,8 +19,48 @@ export const useFetchUserProfile = () => {
 			});
 			if (!response.ok) throw new Error("error in fetch user profile");
 			const result = (await response.json()) as { profile: Profile };
-			console.log("result:", result);
 			return result;
+		},
+	});
+};
+
+export const useUpdateUserProfile = ({
+	firstName,
+	lastName,
+	onError,
+	onSuccess,
+	userName,
+}: {
+	userName: string;
+	lastName: string;
+	firstName: string;
+	onSuccess: () => void;
+	onError: () => void;
+}) => {
+	const { getToken } = useAuth();
+	const queryClient = useQueryClient();
+	return useMutation({
+		mutationFn: async () => {
+			const token = await getToken();
+			const response = await fetch(backendProxyUrl + "/api/auth/user", {
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify({ userName, lastName, firstName }),
+			});
+
+			if (!response.ok) throw new Error("error in update user profile");
+			const result = await response.json();
+			console.log(result);
+		},
+		onSuccess: () => {
+			queryClient.invalidateQueries({ queryKey: ["fetchUserProfile"] });
+			onSuccess();
+		},
+		onError: () => {
+			onError();
 		},
 	});
 };
--- components/myAccount/UpdateMyInfo.tsx
+++ components/myAccount/UpdateMyInfo.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useUpdateUserProfile } from "@/api/userApi";
+import { useEffect, useState } from "react";
 import { Modal, Text, TextInput, View } from "react-native";
+import Toast from "react-native-toast-message";
 import MyButton from "../ui/MyButton";
 
 const InputField = ({
@@ -39,13 +41,93 @@ export default function UpdateMyInfo({
 	const [userName, setUserName] = useState(userNameDefault);
 	const [firstName, setFirstName] = useState(firstNameDefault);
 	const [lastName, setLastName] = useState(lastNameDefault);
-	const handleUpdate = () => {};
+	const [fieldChanged, setFieldChanged] = useState(false);
+	const onError = () =>
+		Toast.show({
+			type: "error",
+			text1: "Update failed, please try again",
+			text1Style: { fontSize: 16 },
+		});
+
+	const onSuccess = () => {
+		/* 2s delay for showing the toast */
+		setTimeout(() => setIsShow(false), 2000);
+		Toast.show({
+			type: "success",
+			text1: "Updated!",
+			text1Style: { fontSize: 16 },
+		});
+	};
+
+	const { isPending, mutate } = useUpdateUserProfile({
+		firstName,
+		lastName,
+		userName,
+		onSuccess,
+		onError,
+	});
+
+	useEffect(() => {
+		if (
+			userName !== userNameDefault ||
+			firstName !== firstNameDefault ||
+			lastName !== lastNameDefault
+		) {
+			setFieldChanged(true);
+		} else if (
+			userName === userNameDefault &&
+			firstName === firstNameDefault &&
+			lastName === lastNameDefault
+		) {
+			setFieldChanged(false);
+		}
+	}, [
+		userName,
+		userNameDefault,
+		firstName,
+		firstNameDefault,
+		lastName,
+		lastNameDefault,
+	]);
+
+	const handleUpdate = () => {
+		if (
+			userName === userNameDefault &&
+			firstName === firstNameDefault &&
+			lastName === lastNameDefault
+		) {
+			Toast.show({
+				type: "error",
+				text1: "You did not make any changes",
+				text1Style: { fontSize: 16 },
+			});
+			return;
+		}
+		if (!userName || !lastName || !firstName) {
+			Toast.show({
+				type: "error",
+				text1: "Fields are all required!",
+				text1Style: { fontSize: 16 },
+			});
+			return;
+		}
+
+		/* update logic with the onSuccess and onError Toast*/
+		mutate();
+	};
+	const handleClose = () => {
+		setIsShow(false);
+		setFirstName(firstNameDefault);
+		setLastName(lastNameDefault);
+		setUserName(userNameDefault);
+	};
 	return (
 		<Modal
 			animationType="slide"
 			transparent={false}
 			visible={visible}
 			backdropColor={"rgba(255,255,255,0.2)"}>
+			<Toast />
 			<View className="flex-1 flex justify-center items-center">
 				<View className="bg-white p-10 rounded-lg w-full gap-y-2">
 					<Text className="text-center font-bold text-2xl mb-4">
@@ -70,10 +152,13 @@ export default function UpdateMyInfo({
 					<View className="flex-row justify-between items-center mt-4">
 						<MyButton
 							text="Close"
-							onPress={() => setIsShow(false)}
+							disabled={isPending}
+							onPress={handleClose}
 							wrapperClassName="w-[45%]"
 						/>
 						<MyButton
+							disabled={!fieldChanged || isPending}
+							spinning={isPending}
 							text="Update"
 							wrapperClassName="w-[45%]"
 							bgColor="bg-red-400"
